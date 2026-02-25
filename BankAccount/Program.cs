a43e922db0307b3da4871c7cbfc2ce3d10674581
jeri2779 <solev0025@gmail.com>
2026-02-25 18:15:39 +0900

    2

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,4 +1,67 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+Console.WriteLine("=== 계좌 개설 ===");
+BankAccount ba = new BankAccount("1001","홍길동");
+BankAccount ba2 = new BankAccount("1002","이순신");
+BankAccount.ShowTotalAccounts();
+
+Console.WriteLine("=== 거래 ===");
+ba.Deposit(50000);
+ba2.Deposit(30000);
+ba.Withdraw(200000);
+ba.Withdraw(100000);
+
+Console.WriteLine("");
+
+
+class BankAccount
+{
+    public static int TotalAccounts = 0;
+    public string AccountNumber;
+    public string OwnerName;
+    public int Balance;
+
+
+    public BankAccount(string accountNumber, string ownerName)
+    {
+        AccountNumber = accountNumber;
+        OwnerName = ownerName;
+        Balance = 0;
+        TotalAccounts++;
+        Console.WriteLine($"[{AccountNumber}] {OwnerName} 계좌가 개설되었습니다.");
+    }
+
+
+    public void Deposit(int amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+            Console.WriteLine($"[{AccountNumber}]{amount}원 입금 완료. 잔액: {Balance}원");
+        }
+        else
+        {
+            Console.WriteLine("입금 금액은 0보다 커야 합니다.");
+        }
+    }
+    public void Withdraw(int amount)
+    {
+        if (Balance > 0)
+        {
+            Balance -= amount;
+            Console.WriteLine($"{amount}원 출금 완료. 잔액: {Balance}원");
+        }
+    }
+    public static void ShowTotalAccounts()
+    {
+        int cnt = TotalAccounts;
+        Console.WriteLine($"현재 총 계좌 수: {cnt}");
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine($"[계좌 정보] {AccountNumber} ({OwnerName}) - 잔액: {Balance}원");
+    }
+
+}
--- ProductInventory/Program.cs
+++ ProductInventory/Program.cs
@@ -1,4 +1,66 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("=== 상품 목록 ===");
+Product[] product = new Product[4];
+product[0] = new Product();
+product[0].Stock = 15; 
+product[0].Name = "노트북";
+product[0].Price = 1500000;
+
+product[1] = new Product();
+product[1].Stock = 20;
+product[1].Name = "마우스";
+product[1].Price = 35000;
+
+product[2] = new Product();
+product[2].Stock = 15;
+product[2].Name = "키보드";
+product[2].Price = 89000;
+
+//Product stock = new Product
+//{
+//    Name = "노트북",
+//    Price = 1500000,
+//    Stock = 5
+//};
+
+for (int i = 0; i < product.Length-1; i++)
+{
+    product[i].ShowInfo();
+}
+
+class Product
+{
+    public string Name;
+    public int Price;
+    public int Stock;
+
+    public void AddStock(int quantity)
+    {
+        Name = quantity.ToString();
+        Price = quantity;
+        Stock++;
+
+    }
+    public void Sell(int quantity)
+    {
+
+    }
+
+    public void GetTotalValue()
+    {
+        Price = 0;
+    }
+
+    public override string ToString()
+    {
+        return $"[{Name}] {Price}원 (재고: {Stock}개)";
+    }
+
+    public void ShowInfo()//테스트용
+    {
+        Console.WriteLine($"[{Name}] {Price}원 (재고: {Stock}개)");
+    }
+
+}
